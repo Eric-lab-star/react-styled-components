33ac9b9aa6ed1e343508f4e61cb26a9ade634790
Eric <cyon256@icloud.com>
2022-02-25 17:25:06 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Shape = styled.div`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  background-color: ${(props) => props.bgColor};
+`;
+
+const Rectangle = styled(Shape)``;
+const Triangle = styled(Shape)`
+  border-bottom: 50px solid blue;
+  border-left: 50px solid transparent;
+  border-right: 50px solid transparent;
+`;
+
+const Circle = styled(Shape)`
+  border-radius: 50%;
+`;
+
+const Button = styled.button`
+  background-color: orange;
+  border-style: none;
+  border-radius: 10px;
+  align-self: center;
+  color: white;
+  line-height: 50px;
+`;
+
+const Input = styled.input.attrs({ required: true, type: "text" })`
+  background-color: ${(props) => props.bgColor};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Rectangle bgColor="teal" width="100px" height="100px" />
+      <Triangle />
+      <Circle bgColor="red" width="100px" height="100px" />
+      <Button bgColor="red">Login</Button>
+      <Button as="a">Login</Button>
+      <Input bgColor="red" />
+    </Father>
   );
 }
 
